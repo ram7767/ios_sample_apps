32ff795e7b04e646945035b3f35c4574e9a619ec
ram7767 <rrnram998539@gmail.com>
2024-06-18 14:41:31 +0530

    Initial Commit

---
+++ AdvanceMaps/AdvanceMapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdvanceMapsApp.swift
+//  AdvanceMaps
+//
+//  Created by Softsuave on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AdvanceMapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
